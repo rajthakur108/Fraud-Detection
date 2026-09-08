99e7036c0e3920bc5dc90858d38bcfd60cc7c595
Akhilendra Singh <rajsuryanvanshi2005@gmail.com>
2026-09-08 11:58:19 +0530

    Commit before running formatting  command

--- tests/integration_tests/run.sh
+++ tests/integration_tests/run.sh
@@ -1,18 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+LOCAL_TAG=`date +"%Y-%m-%d-%H-%M"`
+
+export LOCAL_IMAGE_NAME="fraud-detection-service:${LOCAL_TAG}"
 
 docker build \
     -f tests/integration_tests/Dockerfile \
-    -t fraud-prediction-service:v2 \
+    -t ${LOCAL_IMAGE_NAME} \
     .
 
 docker run -d \
     -p 9696:9696 \
     --name fraud-test \
-    fraud-prediction-service:v2
+    ${LOCAL_IMAGE_NAME}
 
 sleep 10
 
 uv run pytest tests/integration_tests/test.py
 
+ERROR_CODE=$?
+
+if [ ${ERROR_CODE} != 0 ]; then
+    docker logs --tail 50 fraud-test
+fi
+
 docker stop fraud-test
 docker rm fraud-test
+
+exit ${ERROR_CODE}
